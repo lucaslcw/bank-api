04221782615f2ec68efc9aea6059c712a8423fbc
Lucas Cavalcante <cavalcantelucasx@gmail.com>
2023-06-30 07:11:35 -0300

    feat: create bank account schema

--- src/@core/infra/db/bank-account/bank-account.schema.ts
+++ src/@core/infra/db/bank-account/bank-account.schema.ts
@@ -1,13 +1,14 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { TransactionSchema } from '../transaction/transaction.schema';
 
 @Entity()
 export class BankAccountSchema {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ type: 'decimal', scale: 2 })
-  balance: number;
-
   @Column({ length: 255, unique: true })
   account_number: string;
+
+  @OneToMany(() => TransactionSchema, (transaction) => transaction.bank_account)
+  transactions: TransactionSchema[];
 }
