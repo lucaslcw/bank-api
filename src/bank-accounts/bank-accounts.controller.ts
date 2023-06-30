70c1d1d1e11399f4794c7d12ffc39e6be8802a37
Lucas Cavalcante <cavalcantelucasx@gmail.com>
2023-06-30 07:09:02 -0300

    feat: bank accounts controller

--- src/bank-accounts/bank-accounts.controller.ts
+++ src/bank-accounts/bank-accounts.controller.ts
@@ -1,26 +1,16 @@
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { CreateBankAccountDto } from './dto/create-bank-account.dto';
 import { BankAccountService } from '../@core/domain/bank-account/bank-account.service';
-import { TransferBankAccountDto } from './dto/transfer-bank-account.dto';
 
 @Controller('bank-accounts')
 export class BankAccountsController {
   constructor(private readonly bankAccountService: BankAccountService) {}
 
+  @HttpCode(201)
   @Post()
   async create(@Body() createBankAccountDto: CreateBankAccountDto) {
     return await this.bankAccountService.create(
       createBankAccountDto.account_number,
-    );
-  }
-
-  @HttpCode(204)
-  @Post('transfer')
-  transfer(@Body() transferBankAccountDto: TransferBankAccountDto) {
-    return this.bankAccountService.transfer(
-      transferBankAccountDto.from,
-      transferBankAccountDto.to,
-      transferBankAccountDto.amount,
     );
   }
 }
