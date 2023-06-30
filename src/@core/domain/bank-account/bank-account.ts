197cdd3700474433ddf4b87f2a6c2a180e3d3ca6
Lucas Cavalcante <cavalcantelucasx@gmail.com>
2023-06-30 07:09:56 -0300

    test: create bank account domain

--- src/@core/domain/bank-account/bank-account.ts
+++ src/@core/domain/bank-account/bank-account.ts
@@ -1,7 +1,6 @@
 import * as crypto from 'crypto';
 
 type BankAccountProps = {
-  balance: number;
   account_number: string;
 };
 
@@ -10,22 +9,6 @@ export class BankAccount {
 
   constructor(public readonly props: BankAccountProps, id?: string) {
     this.id = id ?? crypto.randomUUID();
-  }
-
-  debit(amount: number): void {
-    this.balance -= amount;
-  }
-
-  credit(amount: number): void {
-    this.balance += amount;
-  }
-
-  get balance(): number {
-    return this.props.balance;
-  }
-
-  private set balance(value: number) {
-    this.props.balance = value;
   }
 
   get account_number(): string {
