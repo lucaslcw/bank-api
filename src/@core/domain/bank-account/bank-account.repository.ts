909609cbba5567eca8cce903a8c69f57d551221a
Lucas Cavalcante <cavalcantelucasx@gmail.com>
2023-06-30 07:09:14 -0300

    feat: create bank account repository

--- src/@core/domain/bank-account/bank-account.repository.ts
+++ src/@core/domain/bank-account/bank-account.repository.ts
@@ -2,6 +2,5 @@ import { BankAccount } from './bank-account';
 
 export interface BankAccountRepository {
   insert(bankAccount: BankAccount): Promise<void>;
-  update(bankAccount: BankAccount): Promise<void>;
-  findByAccountNumber(accountNumber: string): Promise<BankAccount>;
+  findById(id: string): Promise<BankAccount>;
 }
