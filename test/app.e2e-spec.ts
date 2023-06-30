59de427867b72faa069280e032a83c946aa5a748
Lucas Cavalcante <cavalcantelucasx@gmail.com>
2023-06-30 07:05:44 -0300

    test: remove e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('BankAccountsController (e2e)', () => {
@@ -13,25 +12,5 @@ describe('BankAccountsController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
-  });
-
-  it('/bank-accounts (POST)', () => {
-    return request(app.getHttpServer())
-      .post('/bank-accounts')
-      .send({
-        account_number: '111',
-      })
-      .expect(201);
-  });
-
-  it('/bank-accounts (POST)', () => {
-    return request(app.getHttpServer())
-      .post('/bank-accounts/transfer')
-      .send({
-        from: '444',
-        to: '222',
-        amount: 100,
-      })
-      .expect(200);
   });
 });
